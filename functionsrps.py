9ad84dd8c302384ef59c705bbec22cb6ac77409e
Matte2322 <71736367+Matte2322@users.noreply.github.com>
2022-02-17 18:51:57 -0800

    Add files via upload

--- functionsrps.py
+++ functionsrps.py
@@ -1,4 +1,6 @@
 import random
+import os
+import sys
 
 def choicePlayer(playerName, myChoice):
     playerName = input("What's your name?: ")
@@ -17,7 +19,13 @@ def computerChoose():
 # declare global variables by using a dictionary
 randomActionC = computerChoose()
 myChoice, playerName = choicePlayer(playerName=True, myChoice=True)
-print(playerName + " has chosen " + myChoice + " while the computer chose " + randomActionC)
+
+def checkStringResponse():
+    print(playerName + " has chosen " + myChoice + " while the computer chose " + randomActionC)
+    if myChoice != str and myChoice == float:
+        return False
+
+print("--------------------------------------------------------------------------------------")
 
 def inAction():
     while True:
@@ -39,14 +47,20 @@ def inAction():
             else:
                 print("YOU LOST BECAUSE THE COMPUTER CHOSE ROCK.")
         else:
-           print("Try again.")
-        
+            print("Try again, the word you've inputed makes me not understand what you're trying to say. \nTry to input a word within the directions.")
+
+        if myChoice.isdigit():
+            print("Do not input a number, input a word instead.")
+        # ask user to play again
         play_again = input("Do you want to play again? Type 'no' if you don't want to play or type 'yes' if you want to: ")
         if play_again == 'no':
             print("See you next time.")
             break
         else:
-            return choicePlayer(playerName=True, myChoice=True) 
+            print("Have fun!")
+            os.execl(sys.executable, os.path.abspath('/home/doormat/wife/functionsrps.py'), *sys.argv)
+            
+            
 
             
 inAction()
